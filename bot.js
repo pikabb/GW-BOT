1a03bf2ab74df91f8ccbd2c273565d940f91e306
Chick3nBob <thagum21@gmail.com>
2018-02-24 23:28:36 -0600

    Lets hope and pray for something good
    Added a lil command for myself.

--- bot.js
+++ bot.js
@@ -1,100 +1,101 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const p = "*" //This is the prefix. You can always change this when ever.
+
 bot.on('ready', () => {
 console.log('successfully launched!')
 });
+
 //who ever the fuck you are messing with our bot. stop
-bot.on('message', message => {
-if (message.author.bot) return;
 
-    if(message.content == '*hey!') {
+bot.on('message', msg => { //Message Handler. PS: Msg is better
+const args = msg.content.slice(p.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase(); //This is a easier way then putting msg.content == all the time
+if (msg.author.bot) return;
 
-        message.channel.sendMessage('Listen!');
+    if(command == 'hey!') {
+        msg.channel.send('Listen!');
     }
 
-    if(message.content == '*pika') {
-
-        message.channel.sendMessage('chu!');
+    if(command == 'pika') {
+        msg.channel.send('chu!');
     }
 
-    if(message.content == '*ian') {
-
-        message.channel.sendMessage('is a fag');
-    }
-    
-    if(message.content == '*person reading') {
-
-        message.channel.sendMessage('is triple gay');
-    }
-
-    if(message.content == '*person above') {
-
-        message.channel.sendMessage('is triple gay');
-    }
-
-    if(message.content == '*person below') {
-
-        message.channel.sendMessage('is triple gay');
-    }
-
-    if(message.content == '*traps') {
-
-        message.channel.sendMessage('are gay');
-    }
-
-    if(message.content == '*trap card') {
-
-        message.channel.sendMessage('https://media.discordapp.net/attachments/355861481742663680/396886574953922601/XHBa71Th.jpg?width=322&height=469');
-    }
-
-    if(message.content == '*gay') {
-
-        message.channel.sendMessage('https://cdn.discordapp.com/attachments/355861481742663680/396887057085104129/latest-1.jpg');
-    }
-
-    if(message.content == '*infinite reverse') {
-
-        message.channel.sendMessage('https://media.discordapp.net/attachments/355861481742663680/396888457257156608/infinite.jpg');
-    }
-
-
-    if(message.content == '*help') {
-
-        message.channel.sendMessage('My current commands are: pika, ian, person above, person below, person reading, traps, trap card, gay, infinite reverse, shit, my avatar, doctora wark, hey, ..... Use `*` before typing in the commands!');
-    }
-
-    if(message.content == '*BEGONE') {
-
-        message.channel.sendMessage('***THOT***');
-    }
-
-    if(message.content == '****RISE PIKA MAN!***') {
-
-        message.channel.sendMessage('PIKA!!!');
-    }
-
-    if(message.content == '*shit') {
-
-        message.channel.sendMessage('https://media.discordapp.net/attachments/342748315315732480/412094393504497664/shirt.gif?width=261&height=468');
+    if(command == 'ian') {
+        msg.channel.send('is a fag');
     }
     
-    if(message.content == '*doctora wark') {
-           
-         message.channel.sendMessage(' You MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOST FEENESH')
+    if(command == 'person reading') {
+        msg.channel.send('is triple gay');
+    }
+
+    if(command == 'person above') {
+        msg.channel.send('is triple gay');
+    }
+
+    if(command == 'person below') {
+        msg.channel.send('is triple gay');
+    }
+
+    if(command == 'traps') {
+        msg.channel.send('are gay');
+    }
+
+    if(command == 'trap card') {
+        msg.channel.send('https://media.discordapp.net/attachments/355861481742663680/396886574953922601/XHBa71Th.jpg?width=322&height=469');
+    }
+
+    if(command == 'gay') {
+        msg.channel.send('https://cdn.discordapp.com/attachments/355861481742663680/396887057085104129/latest-1.jpg');
+    }
+
+    if(command == 'infinite reverse') {
+        msg.channel.send('https://media.discordapp.net/attachments/355861481742663680/396888457257156608/infinite.jpg');
+    }
+
+
+    if(command == 'help') {
+        msg.channel.send('My current commands are: pika, ian, person above, person below, person reading, traps, trap card, gay, infinite reverse, shit, my avatar, doctora wark, hey, ..... Use `` before typing in the commands!');
+    }
+
+    if(command == 'BEGONE') {
+        msg.channel.send('THOT');
+    }
+
+    if(command == 'RISE PIKA MAN!') {
+        msg.channel.send('PIKA!!!');
+    }
+
+    if(command == 'shit') {
+        msg.channel.send('https://media.discordapp.net/attachments/342748315315732480/412094393504497664/shirt.gif?width=261&height=468');
     }
     
-     if(message.content == '*wark') {
-           
-         message.channel.sendMessage(' You MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOST FEENESH')
+    if(command == 'doctora wark') {
+         msg.channel.send(' You MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOST FEENESH')
+    }
+
+     if(command == 'wark') {
+         msg.channel.send(' You MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOST FEENESH')
     }
   
-    if(message.content == '*nathaniel beard go to the office with your father for cussing') {
-
-        message.channel.sendMessage('https://media.discordapp.net/attachments/342748315315732480/412094393504497664/shirt.gif?width=261&height=468');
+    if(command == 'nathaniel beard go to the office with your father for cussing') {
+        msg.channel.send('https://media.discordapp.net/attachments/342748315315732480/412094393504497664/shirt.gif?width=261&height=468');
     }
-      if (message.content == '*my avatar') {
-    message.reply(message.author.avatarURL);
+    
+    if (command == "eval") {
+        if (!msg.author.id == "228349229230325760") rturn msg.channel.send("Command only used by PainFX for testings.")
+        
+              try {
+        const com = eval(message.content.split(" ").slice(1).join(" "));
+        message.channel.send('```\n' + com + '```');
+      } catch(e) {
+        message.channel.send('```js\n' + e + '```');
+      }
+    }
+    
+      if (command == 'my avatar') {
+    msg.reply(msg.author.avatarURL);
       }
 });
 
-bot.login('NDE2MzE1ODIyOTY1Nzg0NTc3.DXCtYw.SSBfHfjwDt0Z2fFz0SO9meDikyU')
+bot.login('NDE2MzE1ODIyOTY1Nzg0NTc3.DXCtYw.SSBfHfjwDt0Z2fFz0SO9meDikyU');
