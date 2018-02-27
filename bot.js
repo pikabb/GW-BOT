5559da131bb882dfc62869c80ad786b92f46a7d6
Chick3nBob <thagum21@gmail.com>
2018-02-26 21:18:35 -0600

    Moved around some stuff

--- bot.js
+++ bot.js
@@ -13,6 +13,11 @@ pika.on('message', msg => {
 
   // Global Settings - Prefix the can be changed
   db.fetchObject(`guildPrefix_${msg.guild.id}`).then(i => {
+    
+      // Variables - Variables make it easy to call things, since it requires less typing.
+  let sender = msg.author; // This variable takes the message, and finds who the author is.
+  let args = msg.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+  let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
 
     let prefix;
 
@@ -22,11 +27,6 @@ pika.on('message', msg => {
       prefix = '*'
     }
   })
-
-  // Variables - Variables make it easy to call things, since it requires less typing.
-  let sender = msg.author; // This variable takes the message, and finds who the author is.
-  let args = msg.content.slice(prefix.length).trim().split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
-  let cmd = args.shift().toLowerCase(); // This takes away the first object in the cont array, then puts it in this.
 
   if (cmd === "ping") {
     msg.chanel.send(client.ping + "!")
