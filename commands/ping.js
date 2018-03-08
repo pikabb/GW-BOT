9a767e7918e4f16d3350c6f3877eda33bb9f5e55
pikabb <34977374+pikabb@users.noreply.github.com>
2018-03-07 21:04:25 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@
 exports.run = (pika, msg, args, func) => {
     msg.channel.send(pika.ping)
 }
- if (message.content.startsWith(prefix + "hey!")) {
+ if (message.content == "hey!") {
      
     message.channel.sendMessage('Listen!');
   }
